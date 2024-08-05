7516f1bf524d470766bbad17d660f70adc13d866
Kaiky <kaikymss@gmail.com>
2024-08-04 21:43:34 -0300

    Criação da tela de sobre mim

---
+++ assets/js/hardskill.js
@@ -0,0 +1,28 @@
+const skillText = document.getElementById("skill");
+const buttons = [
+    { id: "html", text: " HTML" },
+    { id: "css", text: " CSS" },
+    { id: "js", text: " JavaScript" },
+    { id: "php", text: " PHP" },
+    { id: "mysql", text: " MySQL" },
+    { id: "git", text: " Git" },
+    { id: "github", text: " Github" },
+    { id: "figma", text: " Figma" },
+    { id: "office", text: " Pacote Office" },
+];
+
+buttons.forEach(button => {
+    const element = document.getElementById(button.id);
+    
+    element.addEventListener('mouseover', () => {
+        skillText.textContent = button.text;
+    });
+
+    element.addEventListener('mouseout', () => {
+        skillText.textContent = "";
+    });
+
+    element.addEventListener('click', () => {
+        skillText.textContent = button.text;
+    });
+});
